e46f4a918a9f3596e2b153c8f3112ee1eda8110d
nixon100 <nixon6637@gmail.com>
2024-08-02 10:54:11 +0530

    backup

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ import express from "express";
 import mysql from "mysql2";
 import cors from "cors";
 import multer from "multer";
-import requestIp from "request-ip"
+import requestIp from "request-ip";
 
 const upload = multer();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(requestIp.mw());
-
-
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -22,10 +20,29 @@ const db = mysql.createConnection({
 app.get("/ll", function (req, res) {
   console.log(req.socket.remoteAddress);
   console.log(req.ip);
-  console.log(req.port)
-  res.send("your IP is: " + req.ip +   req.socket.remoteAddress);
+  console.log(req.port);
+  const clientIp = requestIp.getClientIp(req);
+  console.log(clientIp);
+  res.send("your IP is: " + req.ip + req.socket.remoteAddress);
 });
 
+// const allowedIPs = ['::ffff:172.19.0.3'];
+
+// app.use((req, res, next) => {
+//   // const clientIp = req.ip || req.headers['x-forwarded-for'];
+//   // const clientIp = req.ip;
+//   // console.log(clientIp)
+//   fetch('https://api.ipify.org/')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error));
+
+//   if (allowedIPs.includes(data)) {
+//     next();
+//   } else {
+//     res.status(403).send('Access denied');
+//   }
+// });
 
 // const allowedIPs = ['192.168.1.100', '192.168.1.101'];
 // const allowedIPs = ['::1'];
@@ -39,7 +56,6 @@ app.get("/ll", function (req, res) {
 //     res.status(403).send('Access denied');
 //   }
 // });
-
 
 // const firewall = (req, res, next) => {
 //   const allowedIps = ['192.168.1.100', '192.168.1.200']; // allowed IP addresses
@@ -58,15 +74,53 @@ app.get("/ll", function (req, res) {
 
 // app.use(firewall);
 
-
-
-
 // SELECT test.table1.ROLL_NO,test.table2.COURSE_ID,test.table3.SUBJECT , test.table3.MARK
 //   FROM test.table1
 //   JOIN test.table2
 //   ON test.table1.ROLL_NO = test.table2.ROLL_NO
 //   JOIN test.table3
 //   ON test.table2.COURSE_ID = test.table3.COURSE_ID
+///////////////////////////////////////////////////////////////////   ///////////////////////////////////////
+// const allowedIps = ['192.168.1.100', '192.168.1.200'];
+// app.use((req, res, next) => {
+//   app.post("/auth", (req, res) => {
+//     // req.body contains the request body
+//     const ip = req.body;
+//     console.log(ip)
+//     // Do something with the data, e.g., save it to a database
+//     // res.send(`User created: ${name} (${age})`);
+//     if (ip == "192.168.1.100") {
+//       next();
+//     } else {
+//       res.status(403).send("Access denied");
+//     }
+//   });
+
+// });
+app.use((req, res, next) => {
+  const ip = req.ip; // Get the IP from the request object
+  console.log(ip);
+
+  if (ip === "192.168.1.100") {
+    next();
+  } else {
+    res.status(403).send("Access denied"+ip);
+  }
+});
+
+  app.post("/auth", (req, res) => {
+    // req.body contains the request body
+    const ip = req.body;
+    console.log(ip)
+    // Do something with the data, e.g., save it to a database
+    // res.send(`User created: ${name} (${age})`);
+    if (ip == "192.168.1.100") {
+      console.log(ip)
+    } else {
+      res.status(403).send("Access denied");
+    }
+  });
+
 
 
 
@@ -74,7 +128,7 @@ app.get("/ll", function (req, res) {
 
 
 app.get("/table-join", (req, res) => {
-  const q = `call test.get_student_marks()`
+  const q = `call test.get_student_marks()`;
   db.query(q, (err, data) => {
     if (err) {
       console.log(err);
@@ -171,7 +225,6 @@ app.get("/table-join", (req, res) => {
 //   });
 // });
 
-
 ///////////////testing/////////////////
 app.get("/testingdatabase", (req, res) => {
   const q = "SELECT * FROM test.referencevaluea";
@@ -184,42 +237,30 @@ app.get("/testingdatabase", (req, res) => {
   });
 });
 
-
-
-
-
-
-
-
-
 ///////////////////////////////////////////////////////////////////////
 app.post("/upload", (req, res) => {
-  const {data,ref}= req.body;
-if (data.length > 0) {
-console.log(data,ref)
-// console.log(json.get('selectedValue'))
-const values = data.map((obj) => Object.values(obj));
-const placeholders = data.map(() => "(?,?,?,?)").join(", ");
-const query = `INSERT INTO ${ref} VALUES ${placeholders}`;
-db.query(query, values.flat(), (err, results) => {
-  if (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Error inserting data' });
+  const { data, ref } = req.body;
+  if (data.length > 0) {
+    console.log(data, ref);
+    // console.log(json.get('selectedValue'))
+    const values = data.map((obj) => Object.values(obj));
+    const placeholders = data.map(() => "(?,?,?,?)").join(", ");
+    const query = `INSERT INTO ${ref} VALUES ${placeholders}`;
+    db.query(query, values.flat(), (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ message: "Error inserting data" });
+      } else {
+        // console.log(query);
+        res.json({ message: "Data inserted successfully" });
+      }
+    });
   } else {
-    // console.log(query);
-    res.json({ message: 'Data inserted successfully' });
+    res.status(500).json({ message: "Error inserting data" });
   }
-})
-}else{
-  res.status(500).json({ message: 'Error inserting data' });
-}
 });
 
-
-
-
 ///////////////////////////////////////////////////////////////////////////////////////////
-
 
 // app.post("/upload", (req, res) => {
 // if (req.body.length > 0) {
@@ -230,7 +271,6 @@ db.query(query, values.flat(), (err, results) => {
 //       return;
 //     }
 
-
 //   const json = req.body; // assuming the data is sent in the request body
 //   const create = `CREATE TABLE IF NOT EXISTS myntra_reversefees22 (
 //     Reverse_Logistics_CPU VARCHAR(512),
@@ -238,7 +278,7 @@ db.query(query, values.flat(), (err, results) => {
 //     __EMPTY_1 INT,
 //     __EMPTY_2 INT
 //   )`;
-  
+
 //   // Create the table only if it doesn't exist
 //   db.query(create, (err, results) => {
 //     if (err) {
@@ -475,7 +515,3 @@ app.get("/amazonshippingfees", (req, res) => {
 app.listen(8800, () => {
   console.log("Connected to backend.");
 });
-
-
-
-
